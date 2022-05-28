220861b02b32a7eea482e05b8ce59d103c511fbb
Kyaw Htet Paing WIn <kyawhtetpaing111@gmail.com>
2022-05-28 15:45:51 -0700

    Update postings.py

--- indexes/postings.py
+++ indexes/postings.py
@@ -1,7 +1,7 @@
 class Posting:
     """A Posting encapulates a document ID associated with a search query component."""
-    def __init__(self, doc_id : int, position: int):
+    def __init__(self, doc_id : int, position:int =-1):
         self.doc_id = doc_id
-        self.positions = [position]
+        self.positions = [] if position == -1 else [position]
     def __str__(self):
         return f"(ID: {self.doc_id }, {[pos for pos in self.positions]})"
