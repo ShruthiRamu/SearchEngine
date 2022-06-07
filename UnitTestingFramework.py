bbeaa7a165fc822b9e6a16642501a9e3abdbc906
ZPrints <48689811+ZPrints@users.noreply.github.com>
2022-06-06 17:31:14 -0700

    Add soundex and biword tests

--- UnitTestingFramework.py
+++ UnitTestingFramework.py
@@ -1,8 +1,10 @@
 import unittest
 from pathlib import Path
+from time import time_ns
 
+import SoundexIndexer
 from text.newtokenprocessor import NewTokenProcessor
-from textmain import index_corpus
+from main import index_corpus
 from documents import DocumentCorpus, DirectoryCorpus
 from queries import BooleanQueryParser
 
@@ -11,19 +13,6 @@ class TestSearchEngine(unittest.TestCase):
     maxDiff = None
 
     def setUp(self):
-        """self.petdict = {'cat': (('pets1', [1]), ('pets3', [3])),
-                        'jump': [('pets1', [2]), ('pets4', [5])],
-                        'high': [('pbts1', [3])],
-                        'dog': [('pets2', [1]), ('pets3', [1])],
-                        'run': [('pets2', [2]), ('pets4', [3])],
-                        'fast': [('pets2', [3])],
-                        'and': [('pets3', [2]), ('pets4', [4, 6])],
-                        'are': [('pets3', [4])],
-                        'pet': [('pets3', [5]), ('pets4', [1]), ('pets5', [1, 4])],
-                        'should': [('pets4', [2]), ('pets5', [2])],
-                        'play': [('pets4', [7])],
-                        'have': [('pets5', [3])]
-                        }"""
         self.petdict = {'cat': ((0, [1]), (2, [3])),
                         'jump': [(0, [2]), (3, [5])],
                         'high': [(0, [3])],
@@ -47,64 +36,210 @@ class TestSearchEngine(unittest.TestCase):
         self.assertEqual(self.token_processor.process_token("!ad'am''anTIn'e#")[0], "adamantin")
 
     def test_tokenizer2(self):
-        self.assertListEqual(self.token_processor.process_token("Hewlett-Packard"), ['hewlettpackard', 'hewlett', 'packard'])
+        self.assertListEqual(self.token_processor.process_token("Hewlett-Packard"),
+                             ['hewlettpackard', 'hewlett', 'packard'])
 
     def test_tokenizer3(self):
-        self.assertListEqual(self.token_processor.process_token('-F""ree"-for-"all!'), ['freeforall', 'free', 'for', 'all'])
+        self.assertListEqual(self.token_processor.process_token('-F""ree"-for-"all!'),
+                             ['freeforall', 'free', 'for', 'all'])
 
     def test_pii1(self):
-        self.assertEqual(self.index.get_postings('high')[0].positions, self.petdict['high'][0][1])
+        self.assertEqual(self.index[0].get_postings('high')[0].positions, self.petdict['high'][0][1])
 
     def test_pii2(self):
-        self.assertEqual(self.index.get_postings('and')[1].positions, self.petdict['and'][1][1])
+        self.assertEqual(self.index[0].get_postings('jump')[0].positions, self.petdict['jump'][0][1])
 
     def test_pii3(self):
-        self.assertEqual(self.index.get_postings('pet')[2].positions, self.petdict['pet'][2][1])
+        self.assertEqual(self.index[0].get_postings('jump')[1].positions, self.petdict['jump'][1][1])
+
+    def test_pii4(self):
+        self.assertEqual(self.index[0].get_postings('play')[1].positions, self.petdict['play'][1][1])
+
+    def test_pii5(self):
+        self.assertEqual(self.index[0].get_postings('should')[2].positions, self.petdict['should'][2][1])
+
+    def test_pii6(self):
+        self.assertEqual(self.index[0].get_postings('pet')[3].positions, self.petdict['pet'][3][1])
+
+    def test_pii7(self):
+        self.assertEqual(self.index[0].get_postings('and')[1].positions, self.petdict['and'][1][1])
+
+    def test_pii8(self):
+        self.assertEqual(self.index[0].get_postings('pet')[2].positions, self.petdict['pet'][2][1])
 
     def test_phrase(self):
         querycomponent = self.booleanqueryparser.parse_query(query='"pets should play"')
-        postings = querycomponent.get_postings(self.index, token_processor=self.token_processor)
+        postings = querycomponent.get_postings(self.index[0], token_processor=self.token_processor)
         self.assertTrue(len(postings) == 1)
 
     def test_phrase2(self):
         querycomponent = self.booleanqueryparser.parse_query(query='"pets should run and jump and play"')
-        postings = querycomponent.get_postings(self.index, token_processor=self.token_processor)
+        postings = querycomponent.get_postings(self.index[0], token_processor=self.token_processor)
         self.assertTrue(len(postings) == 1)
 
     def test_and(self):
         querycomponent = self.booleanqueryparser.parse_query(query='pets should play')
-        postings = querycomponent.get_postings(self.index, token_processor=self.token_processor)
+        postings = querycomponent.get_postings(self.index[0], token_processor=self.token_processor)
         self.assertTrue(len(postings) == 2)
 
     def test_and2(self):
         querycomponent = self.booleanqueryparser.parse_query(query='cat run')
-        postings = querycomponent.get_postings(self.index, token_processor=self.token_processor)
+        postings = querycomponent.get_postings(self.index[0], token_processor=self.token_processor)
         self.assertTrue(len(postings) == 0)
+
+    def test_and3(self):
+        querycomponent = self.booleanqueryparser.parse_query(query='pets should play run jump')
+        postings = querycomponent.get_postings(self.index[0], token_processor=self.token_processor)
+        self.assertTrue(len(postings) == 1)
 
     def test_or(self):
         querycomponent = self.booleanqueryparser.parse_query(query='cats + dog + pet')
-        postings = querycomponent.get_postings(self.index, token_processor=self.token_processor)
+        postings = querycomponent.get_postings(self.index[0], token_processor=self.token_processor)
         self.assertTrue(len(postings) == 6)
 
     def test_or2(self):
         querycomponent = self.booleanqueryparser.parse_query(query='should + run + and')
-        postings = querycomponent.get_postings(self.index, token_processor=self.token_processor)
+        postings = querycomponent.get_postings(self.index[0], token_processor=self.token_processor)
         self.assertTrue(len(postings) == 5)
+
+    def test_or3(self):
+        querycomponent = self.booleanqueryparser.parse_query(query='rat + hamster + bird')
+        postings = querycomponent.get_postings(self.index[0], token_processor=self.token_processor)
+        self.assertTrue(len(postings) == 0)
 
     def test_not(self):
         querycomponent = self.booleanqueryparser.parse_query(query='pets -should')
-        postings = querycomponent.get_postings(self.index, token_processor=self.token_processor)
+        postings = querycomponent.get_postings(self.index[0], token_processor=self.token_processor)
         self.assertTrue(len(postings) == 1)
+
+    def test_not2(self):
+        querycomponent = self.booleanqueryparser.parse_query(query='-run dogs')
+        postings = querycomponent.get_postings(self.index[0], token_processor=self.token_processor)
+        self.assertTrue(len(postings) == 1)
+
+    def test_not3(self):
+        querycomponent = self.booleanqueryparser.parse_query(query='should -jump -run')
+        postings = querycomponent.get_postings(self.index[0], token_processor=self.token_processor)
+        self.assertTrue(len(postings) == 2)
+
+    def test_not4(self):
+        querycomponent = self.booleanqueryparser.parse_query(query='cat -jump -dog')
+        postings = querycomponent.get_postings(self.index[0], token_processor=self.token_processor)
+        self.assertTrue(len(postings) == 0)
 
     def test_mix(self):
         querycomponent = self.booleanqueryparser.parse_query(query='should run + dogs')
-        postings = querycomponent.get_postings(self.index, token_processor=self.token_processor)
+        postings = querycomponent.get_postings(self.index[0], token_processor=self.token_processor)
         self.assertTrue(len(postings) == 3)
 
     def test_mix2(self):
-        querycomponent = self.booleanqueryparser.parse_query(query='cat + pets -dogs')
-        postings = querycomponent.get_postings(self.index, token_processor=self.token_processor)
-        self.assertTrue(len(postings) == 5)
+        querycomponent = self.booleanqueryparser.parse_query(query='cat + run -dogs')
+        postings = querycomponent.get_postings(self.index[0], token_processor=self.token_processor)
+        self.assertTrue(len(postings) == 3)
+
+    def test_mix3(self):
+        querycomponent = self.booleanqueryparser.parse_query(query='cat + should + fast + run -dogs')
+        postings = querycomponent.get_postings(self.index[0], token_processor=self.token_processor)
+        self.assertTrue(len(postings) == 6)
+
+    def test_mix4(self):
+        querycomponent = self.booleanqueryparser.parse_query(query='cat -dog -jump + dog -cat -fast + pets -should -are')
+        postings = querycomponent.get_postings(self.index[0], token_processor=self.token_processor)
+        self.assertTrue(len(postings) == 0)
+
+    def test_mix5(self):
+        querycomponent = self.booleanqueryparser.parse_query(query='cat -dog -jump + dog -cat -fast + pets -should')
+        postings = querycomponent.get_postings(self.index[0], token_processor=self.token_processor)
+        self.assertTrue(len(postings) == 1)
+
+    def test_soundex(self):
+        print("Soundex Indexing...")
+        soundex_indexer_dir = 'mlb-articles-4000'
+        corpus_path = Path(soundex_indexer_dir)
+        soundex_corpus = DirectoryCorpus.load_json_directory(corpus_path, ".json")
+        start = time_ns()
+        self.i_index, self.soundex_index = SoundexIndexer.index_corpus(soundex_corpus)
+        end = time_ns()
+        print(f"Soundex Indexing Time: {(end - start) / 1e+9} secs\n")
+        authors = SoundexIndexer.soundex_indexer(query='Bryan', index=self.i_index,
+                                                 soundex_index=self.soundex_index)
+        author_list = []
+        for a in authors:
+            author_list.append(a.__str__())
+        print(author_list)
+        self.assertTrue(author_list.__contains__("brian"))
+
+    def test_soundex2(self):
+        print("Soundex Indexing...")
+        soundex_indexer_dir = 'mlb-articles-4000'
+        corpus_path = Path(soundex_indexer_dir)
+        soundex_corpus = DirectoryCorpus.load_json_directory(corpus_path, ".json")
+        start = time_ns()
+        self.i_index, self.soundex_index = SoundexIndexer.index_corpus(soundex_corpus)
+        end = time_ns()
+        print(f"Soundex Indexing Time: {(end - start) / 1e+9} secs\n")
+        authors = SoundexIndexer.soundex_indexer(query='Richrd', index=self.i_index,
+                                                 soundex_index=self.soundex_index)
+        author_list = []
+        for a in authors:
+            author_list.append(a.__str__())
+        print(author_list)
+        self.assertTrue(author_list.__contains__("richard"))
+
+    def test_soundex3(self):
+        print("Soundex Indexing...")
+        soundex_indexer_dir = 'mlb-articles-4000'
+        corpus_path = Path(soundex_indexer_dir)
+        soundex_corpus = DirectoryCorpus.load_json_directory(corpus_path, ".json")
+        start = time_ns()
+        self.i_index, self.soundex_index = SoundexIndexer.index_corpus(soundex_corpus)
+        end = time_ns()
+        print(f"Soundex Indexing Time: {(end - start) / 1e+9} secs\n")
+        authors = SoundexIndexer.soundex_indexer(query='merican', index=self.i_index,
+                                                 soundex_index=self.soundex_index)
+        author_list = []
+        for a in authors:
+            author_list.append(a.__str__())
+        print(author_list)
+        self.assertTrue(author_list.__contains__("merkin"))
+
+    def test_soundex4(self):
+        print("Soundex Indexing...")
+        soundex_indexer_dir = 'mlb-articles-4000'
+        corpus_path = Path(soundex_indexer_dir)
+        soundex_corpus = DirectoryCorpus.load_json_directory(corpus_path, ".json")
+        start = time_ns()
+        self.i_index, self.soundex_index = SoundexIndexer.index_corpus(soundex_corpus)
+        end = time_ns()
+        print(f"Soundex Indexing Time: {(end - start) / 1e+9} secs\n")
+
+        authors = SoundexIndexer.soundex_indexer(query='Berri', index=self.i_index,
+                                                 soundex_index=self.soundex_index)
+        author_list = []
+        for a in authors:
+            author_list.append(a.__str__())
+        print(author_list)
+        self.assertTrue(author_list.__contains__("berry") and author_list.__contains__("berra"))
+
+    def test_biword(self):
+        querycomponent = self.booleanqueryparser.parse_query(query='"pets should"')
+        postings = querycomponent.get_postings(self.index[1], token_processor=self.token_processor)
+        self.assertTrue(len(postings) == 3)
+
+    def test_biword2(self):
+        querycomponent = self.booleanqueryparser.parse_query(query='"dogs run"')
+        postings = querycomponent.get_postings(self.index[1], token_processor=self.token_processor)
+        self.assertTrue(len(postings) == 1)
+
+    def test_biword3(self):
+        querycomponent = self.booleanqueryparser.parse_query(query='"dogs run fast"')
+        postings = querycomponent.get_postings(self.index[1], token_processor=self.token_processor)
+        self.assertTrue(len(postings) == 0)
+
+    def test_biword4(self):
+        querycomponent = self.booleanqueryparser.parse_query(query='"dogs"')
+        postings = querycomponent.get_postings(self.index[1], token_processor=self.token_processor)
+        self.assertTrue(len(postings) == 0)
 
 
 if __name__ == '__main__':
